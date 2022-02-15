683e2a107e4606c0e3873bdc6d406ff50b05027f
jessicaly <jessiclyaruiz@hotmail.com>
2022-02-14 20:15:04 -0500

    taller 29

---
+++ CorrecionPrueba2/src/main/java/ec/edu/uce/CorrecionPrueba2Application.java
@@ -0,0 +1,13 @@
+package ec.edu.uce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrecionPrueba2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrecionPrueba2Application.class, args);
+	}
+
+}
